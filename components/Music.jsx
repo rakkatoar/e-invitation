1155bdd8116b3653e281b8a9fde12039d3ee3607
Rakka Toar <rakkatoar@gmail.com>
2022-04-10 07:15:26 +0700

    fix music button

--- components/Music.jsx
+++ components/Music.jsx
@@ -3,20 +3,21 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import YouTube from 'react-youtube';
 const Music = () => {
-	const [playing, setPlaying] = useState(true);
+	const [playing, setPlaying] = useState(false);
 	const opts = {
 	height: '390',
 	width: '640',
 	playerVars: {
 		autoPlay: 1,
 		mute: 0,
-		loop:1
+		// loop:1
 	},
 };
 	let player;
 	const _onReady = (event) =>  {
 		event.target.seekTo(1, 'seconds');
 		event.target.playVideo();
+		setPlaying(true);
   }
 
 	const setPlay = (event) =>  {
@@ -49,7 +50,7 @@ const Music = () => {
 
   return (
         <div className="">
-					<div className="bg-cultured fixed bottom-5 left-5 rounded-full border-2 p-3">
+					<div className="bg-cultured fixed bottom-5 left-5 rounded-full border-2 p-3 z-20">
 						<div className="relative">
 							{playing &&  <button onClick={toogleVideo} className="delay-1000"><Image
 													src="/img/pause-icon.png"
@@ -67,7 +68,7 @@ const Music = () => {
 					</div>
 					<div className="hidden">
 						{/* <YouTube videoId="1ieSEOX5OEE" opts={opts} onReady={_onReady} onPlay={setPlayer} onPause={setPlayer}  /> */}
-						<YouTube videoId="1ieSEOX5OEE" opts={opts} onReady={_onReady} onPlay={setPlay} onPause={setPause}  />
+						<YouTube videoId="1ieSEOX5OEE" opts={opts} onReady={_onReady} onEnded={setPlay} onPlay={setPlay} onPause={setPause}  />
 					</div>
         </div>
     );
